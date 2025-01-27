e20650bafcc861a7e04ec0b119622a2af9c0bbb1
Osvaldo Javier Arrua Maidana <osvaldoarrua.p@hotmail.com>
2025-01-27 15:07:57 +0100

    version  1.1

--- src/js/app.js
+++ src/js/app.js
@@ -31,10 +31,12 @@ function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes')
     
     for(let i = 1; i <= CANTIDAD_IMAGENES; i++) {
-        const imagen = document.createElement('IMG')
-        imagen.loading = 'lazy'
-        imagen.src = `src/img/gallery/full/${i}.jpg`
-        imagen.alt = 'Imagen Galería'
+        const imagen = document.createElement('PICTURE') 
+        imagen.innerHTML = `
+        <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
+        <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+    `;
 
 
         // Event Handler
@@ -50,12 +52,12 @@ function crearGaleria() {
 }
 
 function mostrarImagen(i) {
-    const imagen = document.createElement('IMG')
-        imagen.src = `src/img/gallery/full/${i}.jpg`
-        imagen.alt = 'Imagen Galería'
-
-
-
+    const imagen = document.createElement('PICTURE')
+    imagen.innerHTML = `
+    <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
+    <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+`;
 
     // Generar Modal
 
